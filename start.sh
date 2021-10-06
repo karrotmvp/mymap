66e682d4f6ab56403406094f1c2ad2c68a743267
rycando <rycando@naver.com>
2021-10-06 23:38:49 +0900

    [chore] CI/CD 파일 생성

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /deploy/mymap/server
+
+pm2 set pm2-slack:slack_url 
+pm2 set pm2-slack:servername mymap
+pm2 set pm2-slack:start true
+pm2 set pm2-slack:stop true
+pm2 set pm2-slack:restart true
+pm2-runtime start ./dist/src/main.js
