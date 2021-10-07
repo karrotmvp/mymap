da420165b5dcea4d2f148bbbcfc0d67eb3fbc941
lee jeongmin <danmin20@khu.ac.kr>
2021-10-07 17:08:16 +0900

    [chore] stylesheet

---
+++ client/src/styles/globalStyle.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import "./reset.scss";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+
+    :focus {
+        outline: none;
+        border: none;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    html{
+        font-size: 10px;
+        -webkit-text-size-adjust: none;
+        font-family: 'Spoqa Han Sans Neo', 'sans-serif';   
+        font-display: fallback;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+`;
