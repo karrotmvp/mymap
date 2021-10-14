b1647cab89cb33760d7bbfcb98628ed3ab809f51
Rudy <rycando@naver.com>
2021-10-14 18:06:27 +0900

    [chore] cors 셋팅 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,11 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   await app.listen(3000);
 }
 bootstrap();
