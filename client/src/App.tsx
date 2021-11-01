99dbadbb18895adad6fea02faf51884a495ababe
lee jeongmin <danmin20@khu.ac.kr>
2021-11-01 16:13:04 +0900

    [fix] 초기 진입 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Around from "./Pages/Around/Around";
@@ -5,9 +6,9 @@ import Mypage from "./Pages/Mypage";
 import Detail from "./Pages/Detail";
 import Write from "./Pages/Write/Write";
 import Mini from "@karrotmarket/mini";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { ViewerInfo, RegionId } from "./Shared/atom";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { getLogin } from "./api/user";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
@@ -20,61 +21,67 @@ dayjs.locale("ko");
 
 const mini = new Mini();
 
+const preload = new URLSearchParams(window.location.search).get("preload");
+const regionId = new URLSearchParams(window.location.search).get("region_id");
+const code = new URLSearchParams(window.location.search).get("code");
+
 function App() {
   const setRegionId = useSetRecoilState(RegionId);
 
   // 로그인 및 내 정보 저장
-  const setMyInfo = useSetRecoilState(ViewerInfo);
-  const getMyInfo = useCallback(
-    async (code: string, regionId: string) => {
-      const data = await getLogin(code, regionId);
-      setMyInfo({
-        userId: data.userId,
-        userName: data.userName,
-        regionName: data.regionName,
-      });
-      localStorage.setItem("token", data.token);
-      Mixpanel.identify(data.userId.toString());
-    },
-    [setMyInfo]
-  );
+  const [viewerInfo, setViewerInfo] = useRecoilState(ViewerInfo);
+  const getMyInfo = useCallback(async (code: string, regionId: string) => {
+    const data = await getLogin(code, regionId);
+    setViewerInfo({
+      userId: data.userId,
+      userName: data.userName,
+      regionName: data.regionName,
+    });
+    localStorage.setItem("token", data.token);
+    Mixpanel.identify(data.userId.toString());
+  }, []);
 
-  mini.startPreset({
-    preset:
-      process.env.NODE_ENV === "production"
-        ? (process.env.REACT_APP_LOGIN as string)
-        : "https://mini-assets.kr.karrotmarket.com/presets/common-login/alpha.html",
-    params: {
-      appId: process.env.REACT_APP_APP_ID as string,
-    },
-    onSuccess: function (result) {
-      if (result && result.code) {
-        const regionId = new URLSearchParams(window.location.search).get(
-          "region_id"
-        );
-
-        getMyInfo(result.code, regionId as string);
-        setRegionId(regionId as string);
+  useEffect(() => {
+    if (!preload) {
+      setRegionId(regionId as string);
+      if (code) getMyInfo(code, regionId as string);
+      else {
+        mini.startPreset({
+          preset:
+            process.env.NODE_ENV === "production"
+              ? (process.env.REACT_APP_LOGIN as string)
+              : "https://mini-assets.kr.karrotmarket.com/presets/common-login/alpha.html",
+          params: {
+            appId: process.env.REACT_APP_APP_ID as string,
+          },
+          onSuccess: function (result) {
+            if (result && result.code) {
+              getMyInfo(result.code, regionId as string);
+            }
+          },
+        });
       }
-    },
-  });
+    }
+  }, []);
 
   return (
     <Router>
       <Analytics id="UA-211655411-1" debug>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/401" component={ClosePage} />
-            <Route path="/detail/:postId" component={Detail} />
-            <Route exact path="/around" component={Around} />
-            <Route exact path="/mypage" component={Mypage} />
-            <Route exact path="/write" component={Write} />
-            <Route exact path="/onboarding" component={Onboarding} />
-            <Route exact path="/onboarding/write" component={Write} />
-            <Route path="/edit/:id" component={Write} />
-          </Switch>
-        </div>
+        {viewerInfo.userId && (
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/401" component={ClosePage} />
+              <Route path="/detail/:postId" component={Detail} />
+              <Route exact path="/around" component={Around} />
+              <Route exact path="/mypage" component={Mypage} />
+              <Route exact path="/write" component={Write} />
+              <Route exact path="/onboarding" component={Onboarding} />
+              <Route exact path="/onboarding/write" component={Write} />
+              <Route path="/edit/:id" component={Write} />
+            </Switch>
+          </div>
+        )}
       </Analytics>
     </Router>
   );
