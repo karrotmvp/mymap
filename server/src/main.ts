4b53fe936c5b3faff42e2fc2daab658e6f9215c0
rudy <rudy@daangn.com>
2021-11-04 18:36:26 +0900

    [fix] cors 설정 변겨

--- server/src/main.ts
+++ server/src/main.ts
@@ -37,6 +37,12 @@ async function bootstrap() {
     .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/swagger', app, document)
+  } else {
+    app.enableCors({
+      origin: "admin.daangn-mymap.com",
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    })
   }
   app.useGlobalPipes(new ValidationPipe());
 
