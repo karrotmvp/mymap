07be76dcb9d11230a55cc8e98f8f94d75d3f0c3c
rudy <rudy@daangn.com>
2021-11-12 15:32:43 +0900

    [fix] useEffect import 수정

--- admin/src/components/views/LandingPage/RecommendPage.js
+++ admin/src/components/views/LandingPage/RecommendPage.js
@@ -1,6 +1,5 @@
 import axios from 'axios' 
-import React, { useState } from 'react'
-import { useEffect } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react'
 import './post.css'
 
 function RecommendPage(props) {
