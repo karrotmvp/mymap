124dcdcc119516671068212b9a8e6ed0eb584468
rudy <rudy@daangn.com>
2021-11-15 15:28:31 +0900

    [fix] savedPost 저장시간순

--- server/src/post/post.processor.ts
+++ server/src/post/post.processor.ts
@@ -1,7 +1,11 @@
+import { Process, Processor } from "@nestjs/bull";
 import { Controller } from "@nestjs/common";
 import { EventPattern, Payload } from "@nestjs/microservices";
+import { User } from "@sentry/types";
+import { Job } from "bull";
 import { Event } from "src/event/event";
 import { MyMapEvent } from "src/event/event-pub-sub";
+import { UserService } from "src/user/user.service";
 import { CreatePostDTO } from "./dto/create-post.dto";
 import { PostService } from "./post.service";
 
@@ -21,4 +25,20 @@ export class PostProcessor {
         // newPost.regionId = regionId;
         await this.postService.createPost(userId, newPost);
     }
+}
+
+@Processor('post')
+export class DefaultPostProcessor {
+    constructor(
+        private readonly userService: UserService,
+        private readonly postService: PostService
+    ) {}
+
+    @Process('defaultPost_created')
+    async createDefaultPost(job: Job<Event>) {
+        const userId: number = job.data._id;
+        const user: User = await this.userService.readUser(userId);
+        
+    }
+
 }
